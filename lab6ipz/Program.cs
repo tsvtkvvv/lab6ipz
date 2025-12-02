04a8f44f8fd054a9e1b97575ea980460ad2ece65
tsvtkvvv <125773942+tsvtkvvv@users.noreply.github.com>
2025-12-02 06:55:34 +0200

    Добавьте файлы проекта.

---
+++ lab6ipz/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class ComputerSystem
+{
+    public bool InternetAvailable;
+    public int MemoryAvailable;
+    public int CpuLoad;
+
+    public void CheckState()
+    {
+        Console.WriteLine("Checking system...");
+
+        if (InternetAvailable == true)
+        {
+            Console.WriteLine("Internet OK");
+        }
+        else
+        {
+            Console.WriteLine("No Internet");
+        }
+
+        if (MemoryAvailable < 500)
+        {
+            Console.WriteLine("Low RAM!");
+        }
+        else
+        {
+            Console.WriteLine("RAM normal");
+        }
+
+        if (CpuLoad > 80)
+        {
+            Console.WriteLine("High CPU load!");
+        }
+        else
+        {
+            Console.WriteLine("CPU OK");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ComputerSystem system = new ComputerSystem();
+        system.InternetAvailable = true;
+        system.MemoryAvailable = 300;
+        system.CpuLoad = 63;
+
+        system.CheckState();
+    }
+}
